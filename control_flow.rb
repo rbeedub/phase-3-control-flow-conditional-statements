2f3c892f300c3f2331b4834541da9d082e66493a
Rebecca Wollard <rbwollard@gmail.com>
2023-01-30 11:56:27 -0700

    commit tree bbf9a512eac5aa6a993466c25ad7b7f27668ecb4

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,35 @@
 def admin_login(username, password)
-  # your code here
+  if username = "admin" && password = "12345"
+    "Access granted"
+  elsif username = "ADMIN" && password = "12345"
+    "Access granted"
+  else "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature >= 40 && temperature <= 60
+    "It's a little chilly out there!"
+  elsif temperature >= 85 
+    "It's too dang hot out there!"
+  else 
+    "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num / 3 == Integer && num / 5 = Integer
+    "FizzBuzz"
+  elsif num / 3 == Integer
+    "Fizz"
+  elsif num / 5 == Integer
+    "FizzBuzz"
+  else
+    num
 end
 
 def calculator(operation, num1, num2)
   # your code here
 end
-
